1e3b9b2b26974c7501d5c422744183d68f19dcf5
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-16 04:23:19 +0530

    chore: basic application setup complete
    closes #3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Syne, Space_Mono } from '@next/font/google';
+import Head from 'next/head';
+
+const syne = Syne({
+	style: ['normal'],
+	display: 'swap',
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700', '800'],
+	variable: '--font-syne',
+});
+
+const spaceMono = Space_Mono({
+	style: ['normal'],
+	display: 'swap',
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	variable: '--font-space-mono',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+			</Head>
+			<main
+				className={`${syne.variable} ${spaceMono.variable} font-spaceMono`}
+			>
+				<Component {...pageProps} />
+			</main>
+		</>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        syne: ['var(--font-syne)'],
+        spaceMono: ['var(--font-space-mono)']
       },
+      colors: {
+        primary: '#14094fe6'
+      }
     },
   },
   plugins: [],
