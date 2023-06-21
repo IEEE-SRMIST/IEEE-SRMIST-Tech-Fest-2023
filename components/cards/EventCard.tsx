fd2637d4cbfe6095b0937820a867ecaaab3b546b
ik04 <ishaankhurana123@gmail.com>
2023-06-22 03:59:40 +0530

    feat: create card that can store dynamic content

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -1,5 +1,28 @@
-const EventCard = () => {
-	return <div>EventCard</div>;
+import { Event } from "@/pages/events/types/types";
+
+interface eventProps {
+  event: Event;
+}
+const EventCard = (props: eventProps) => {
+  return (
+    <div className="w-[700px] space-x-10 flex">
+      <div className="image-container w-2/3 bg-black">
+        <img
+          alt="Event-image"
+          className="image h-full"
+          src={props.event.imagePath}
+        />
+      </div>
+      <div className="card-contents flex flex-col w-full">
+        <h1 className="font-syne text-start text-xl font-bold">
+          {props.event.name}
+        </h1>
+        <p className="font-spaceMono text-cardDescription text-xs">
+          {props.event.description}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default EventCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        syne: ['var(--font-syne)'],
-        spaceMono: ['var(--font-space-mono)']
+        syne: ["var(--font-syne)"],
+        spaceMono: ["var(--font-space-mono)"],
       },
       colors: {
-        primary: '#14094fe6'
-      }
+        primary: "#14094fe6",
+        cardDescription: "#B2BEFF",
+      },
     },
   },
   plugins: [],
-}
+};
