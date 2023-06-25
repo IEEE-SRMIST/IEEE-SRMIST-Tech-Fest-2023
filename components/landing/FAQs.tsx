d96f07b1b2eb549787e9a085c430cda41297db3c
Swastik <sr6015@srmist.edu.in>
2023-06-26 01:24:43 +0530

    feat: FAQ, FAQCard, TImeline

--- components/landing/FAQs.tsx
+++ components/landing/FAQs.tsx
@@ -1,5 +1,53 @@
-function FAQs() {
-	return <div>FAQs</div>;
+import React from "react";
+import { useState } from 'react';
+import FAQCard from "../cards/FAQCard";
+
+// Props type
+
+
+const FAQs: React.FC = () => {
+	const [open, setOpen] = useState<number | boolean>(false);
+
+	const toggle = (index: number | boolean) => {
+		if(open === index){
+		 	return setOpen(false);
+		}
+
+		setOpen(index);
+	}
+
+	const accordianData = [
+		{
+			title: "This is demo title 1",
+			desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea laborum facere consectetur consequatur tempore placeat recusandae at magnam modi, ab eaque, corrupti molestias porro consequuntur adipisci? Numquam magnam quisquam earum."
+		},
+		{
+			title: "This is demo title 2",
+			desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea laborum facere consectetur consequatur tempore placeat recusandae at magnam modi, ab eaque, corrupti molestias porro consequuntur adipisci? Numquam magnam quisquam earum."
+		},
+		{
+			title: "This is demo title 3",
+			desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea laborum facere consectetur consequatur tempore placeat recusandae at magnam modi, ab eaque, corrupti molestias porro consequuntur adipisci? Numquam magnam quisquam earum."
+		}
+	]
+
+	return (
+		<div className="min-h-screen grid place-items-center">
+			<div className="px-[40px] max-w-[1200px]">
+				{accordianData.map((data, index) => {
+					return (
+						<FAQCard 
+							key= {index} 
+							open= {index === open} 
+							title= {data.title} 
+							desc= {data.desc} 
+							toggle={() => toggle(index)} 
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);	
 }
 
 export default FAQs;
