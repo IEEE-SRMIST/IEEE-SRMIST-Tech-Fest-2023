e24ea43f324d61d948f4811220eba16419d74008
Ahad1713 <khanazan761@gmail.com>
2023-06-26 20:30:53 +0530

    :art: reform code

--- pages/hackathon.tsx
+++ pages/hackathon.tsx
@@ -1,8 +1,3 @@
-// function hackathon() {
-// 	return <div>hackathon</div>;
-// }
-
-// export default hackathon;
 import React, { useState } from "react";
 
 const FAQ_DATA = [
@@ -34,7 +29,7 @@ const FaqPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 font-mono">
       <div>
-        <h2 className="text-center mt-8 text-6xl md:text-7xl lg:text-8xl">FAQs</h2>
+        <h2 className="text-center mt-8 text-6xl md:text-7xl lg:text-8xl text-purple-100">FAQs</h2>
         <div className="flex flex-col items-center mt-8">
           <div className="w-full md:w-[540px]">
             {FAQ_DATA.map((faq, index) => (
@@ -44,15 +39,17 @@ const FaqPage: React.FC = () => {
               >
                 <div
                   className={`flex items-center justify-between p-4 cursor-pointer bg-purple-100 border border-purple-600 ${
-                    openIndex === index ? "bg-purple-200" : ""
-                  }`}
+                    openIndex === index ? "bg-purple-100" : ""
+                  } transition-colors duration-300`}
                   onClick={() => toggleFaq(index)}
                 >
-                  <span className="text-purple-600">{faq.question}</span>
+                  <span className={`text-purple-600 ${openIndex === index ? "font-semibold" : ""}`}>
+                    {faq.question}
+                  </span>
                   <svg
                     className={`w-6 h-6 transform ${
                       openIndex === index ? "rotate-180" : ""
-                    } text-purple-600`}
+                    } text-purple-600 transition-transform duration-300`}
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -64,7 +61,7 @@ const FaqPage: React.FC = () => {
                 <div
                   className={`p-4 ${
                     openIndex === index ? "block" : "hidden"
-                  } bg-purple-200`}
+                  } bg-purple-300`}
                 >
                   <span className="text-purple-600 break-words">{faq.answer}</span>
                 </div>
