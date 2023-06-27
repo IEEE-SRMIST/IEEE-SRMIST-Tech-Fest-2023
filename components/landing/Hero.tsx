b7ad8b8817d3b434590f0fe6fac53871cf694a1f
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-06-27 15:20:20 +0530

    Hero & Sponsor sections (#43)
    * feat: Created the Coming Soon Page
    
    * feat: countdown
    
    * chore: image url and landing responsiveness
    
    * chore: logo width
    
    * Sponsor & Hero section done
    
    ---------
    
    Co-authored-by: itaintkevin <kpjtofficial@gmail.com>
    Co-authored-by: Kunal Keshan <kunalkeshan12@gmail.com>

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,5 +1,46 @@
+import Countdown, { CountdownRenderProps } from 'react-countdown';
+import { dateWithTimeZone } from '@/utils/date';
+import { useMemo } from 'react';
+
 function Hero() {
-	return <div>Hero</div>;
+
+	const date = useMemo(() => {
+		return dateWithTimeZone('Asia/Kolkata', 2023, 6, 24, 23, 59, 59);
+	}, []);
+	// Renderer callback with condition
+	const renderer = ({
+		days,
+		hours,
+		minutes,
+		completed,
+	}: CountdownRenderProps) => {
+		if (completed) return <span>Time&apos;s up!</span>;
+		else
+			return (
+				<span>
+					{days}D:{hours}H:{minutes}M
+				</span>
+			);
+	};
+
+  return (
+    <div className="relative flex flex-col justify-start items-center w-full min-h-screen">
+      <video id="videoBg" className="-z-10 absolute top-0" autoPlay loop muted>
+        <source src="/assets/video.mp4" />
+      </video>
+      <div className="img mt-40 2xl:mt-60"></div>
+      <p className="text-lg mt-4">PRESENTS</p>
+      <h1 className="md:text-7xl sm:text-5xl text-3xl font-extrabold font-syne mt-20">
+        TechnoZest
+      </h1>
+      <h3 className="md:text-2xl sm:text-xl text-lg text-center px-10">
+        Encounter your canon event across the TechnoVerse
+      </h3>
+      <p className="text-3xl animate-pulse font-extrabold mt-10 flex flex-col sm:flex-row justify-center gap-2 sm:gap-8 items-center">
+        <Countdown date={date} renderer={renderer} />
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
--- components/landing/Sponsors.tsx
+++ components/landing/Sponsors.tsx
@@ -1,5 +1,57 @@
+import Image from "next/image";
+import InfiniteLooper from "@/utils/infinitelooper";
+
 function Sponsors() {
-	return <div>Sponsors</div>;
+  const imageUrls: { [key: number]: string } = {
+    1: "/assets/salesforce.png",
+    2: "/assets/Sharepoint.png",
+    3: "/assets/Documentum.png",
+    4: "/assets/Drive.png",
+    5: "/assets/edgar.png",
+  };
+
+  return (
+    <div className="w-full flex flex-col gap-6 items-center py-4 overflow-hidden">
+      <div className="flex justify-center items-center w-full">
+        <h1 className="text-4xl">SPONSORS</h1>
+      </div>
+
+      <InfiniteLooper speed={10} direction="right">
+        {Object.keys(imageUrls).map((boxNumber: string, index: number) => (
+          <div className="flex bg-transparent" key={index}>
+            {/* Image box */}
+            <div
+              className={`h-[100px] w-[200px] rounded-md px-[3px] pt-[3px] ${
+                parseInt(boxNumber, 10) % 2 === 0
+                  ? "bg-gradient-to-r from-[#f5832c] via-[#edc77c] to-[#efb643]"
+                  : "bg-gradient-to-r from-[#a36fd7] via-[#cc00ff] to-[#a36fd7]"
+              }`}
+            >
+              <div className="h-full w-full bg-primary rounded-md">
+                <div className="flex items-center justify-center h-full w-full space-x-2 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50">
+                  <Image
+                    src={imageUrls[parseInt(boxNumber, 10)]}
+                    alt={`Image ${boxNumber}`}
+                    className="flex p-3 w-full h-full rounded-md object-fill"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Arrow box */}
+            <div className="flex flex-col gap-4 justify-center items-center">
+              <div className="h-[3px] w-16 bg-gradient-to-r from-[#b283e2] via-[#cc00ff] to-[#ae83d8]"></div>
+              {parseInt(boxNumber, 10) % 2 === 0 && (
+                <div className="h-[3px] w-16 bg-gradient-to-r from-[#b283e2] via-[#cc00ff] to-[#ae83d8]"></div>
+              )}
+            </div>
+          </div>
+        ))}
+      </InfiniteLooper>
+    </div>
+  );
 }
 
 export default Sponsors;
