3e52a86e6daa092b8da94d4b21bf5b7f443a1011
Ishaan Khurana <63468587+ik04@users.noreply.github.com>
2023-07-09 18:35:07 +0530

    feat:added responsiveness to events page (#53)
    * feat:added responsiveness to events page
    issue: time bugging out at certain breakpoints (review required)
    
    * feat: fixed responsive links and added close functionality (sets isopen to false on clicking links)
    
    ---------
    
    Co-authored-by: Kunal Keshan <kunalkeshan12@gmail.com>

--- components/events/Timeline.tsx
+++ components/events/Timeline.tsx
@@ -4,23 +4,23 @@ import React from "react";
 
 export default function timeline(props: timelineProps) {
   return (
-    <div className="container bg-transparent mx-auto mt-10w-full h-full">
+    <div className="container bg-transparent mx-auto mt-10 w-full h-full">
       <div className="relative wrap overflow-hidden p-10 h-full">
-        <div className="border-2-2 left-2/4 absolute border-opacity-20 border-white h-full border"></div>
+        <div className="md:border-2-2 md:left-2/4 md:absolute md:border-opacity-20 md:border-white md:h-full md:border"></div>
 
         {props.events.map((event, index) => (
           <div key={index}>
             {index % 2 === 0 ? (
               <>
-                <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                  <div className="order-1 w-5/12"></div>
-                  <div className="z-20 flex items-center order-1 bg-white shadow-xl w-8 h-8 rounded-full">
+                <div className="mb-8 flex md:justify-between space-x-3  items-center md:w-full w-[700px] right-timeline">
+                  <div className="md:order-1 md:w-5/12"></div>
+                  <div className="z-1 flex items-center order-1 bg-white shadow-xl w-8 h-8 rounded-full">
                     <h1 className="mx-auto font-semibold text-sm text-primary">
                       {index + 1}
                     </h1>
                   </div>
                   <div className="order-1 border border-white/25 shadow-white/30 bg-white/10 hover:bg-white/20 backdrop-blur-sm backdrop-opacity-80 rounded-lg transition ease-in-out duration-500 hover:transition hover:ease-in-out hover:duration-500 hover:-translate-y-1 w-5/12 p-6">
-                    <h3 className="mb-3 font-bold text-white text-lg">
+                    <h3 className="mb-3 font-bold text-white md:text-lg">
                       {event.name}
                     </h3>
                     {/* <time>{event.date}</time> */}
@@ -29,7 +29,7 @@ export default function timeline(props: timelineProps) {
                     </p>
                     <Link
                       href={`events/${event.slug}`} // either slug or the uuid
-                      className="inline-flex items-center px-4 py-2 font-bold text-sm text-primary duration-500 bg-white border-2 border-white rounded-lg hover:bg-white/10 hover:text-white focus:z-10 focus:ring-4 focus:outline-none cursor-pointer"
+                      className="inline-flex items-center md:px-4 md:py-2 p-1 font-bold text-xs md:text-sm text-primary duration-500 bg-white border-2 border-white rounded-lg hover:bg-white/10 hover:text-white focus:z-10 focus:ring-4 focus:outline-none cursor-pointer"
                     >
                       Learn more{" "}
                       <svg
@@ -49,15 +49,16 @@ export default function timeline(props: timelineProps) {
                 </div>
               </>
             ) : (
-              <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                <div className="order-1 w-5/12"></div>
-                <div className="z-20 flex items-center order-1 bg-white shadow-xl w-8 h-8 rounded-full">
+              //! issue styles bad under 640px
+              <div className="mb-8 flex md:justify-between space-x-3 flex-row-reverse items-center w-[700px] md:-translate-x-3 sm:-translate-x-[100px] -translate-x-[54%]  md:w-full left-timeline">
+                <div className="md:order-1 md:w-5/12"></div>
+                <div className="z-1 flex items-center order-1 bg-white shadow-xl w-8 h-8 rounded-full">
                   <h1 className="mx-auto text-primary font-semibold text-lg">
                     {index + 1}
                   </h1>
                 </div>
                 <div className="order-1 border border-white/25 shadow-white/30 bg-white/10 hover:bg-white/20 backdrop-blur-sm backdrop-opacity-80 rounded-lg transition ease-in-out duration-500 hover:transition hover:ease-in-out hover:duration-500 hover:-translate-y-1 w-5/12 p-6">
-                  <h3 className="mb-3 font-bold text-white text-lg">
+                  <h3 className="mb-3 font-bold text-white md:text-lg ">
                     {event.name}
                   </h3>
                   {/* <time>{event.date}</time> */}
@@ -66,7 +67,7 @@ export default function timeline(props: timelineProps) {
                   </p>
                   <a
                     href={`events/${event.slug}`}
-                    className="inline-flex items-center px-4 py-2 font-bold text-sm text-primary duration-500 bg-white border-2 border-white rounded-lg hover:bg-white/10 hover:text-white focus:z-10 focus:ring-4 focus:outline-none cursor-pointer"
+                    className="inline-flex items-center md:px-4 md:py-2 p-1 font-bold text-xs md:text-sm text-primary duration-500 bg-white border-2 border-white rounded-lg hover:bg-white/10 hover:text-white focus:z-10 focus:ring-4 focus:outline-none cursor-pointer"
                   >
                     Learn more{" "}
                     <svg
@@ -87,40 +88,6 @@ export default function timeline(props: timelineProps) {
             )}
           </div>
         ))}
-
-        {/* <div className="mb-8 flex justify-between items-center w-full right-timeline">
-          <div className="order-1 w-5/12"></div>
-          <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-            <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
-          </div>
-          <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-            <h3 className="mb-3 font-bold text-gray-800 text-xl">
-              Lorem Ipsum
-            </h3>
-            <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-
-        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-          <div className="order-1 w-5/12"></div>
-          <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-            <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
-          </div>
-          <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-            <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-            <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div> */}
       </div>
     </div>
   );
