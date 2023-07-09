d698e740eb53f7053c7561c0c0ebada2af4f5022
Disha <91051022+disha2301@users.noreply.github.com>
2023-07-09 18:31:11 +0530

    fix: responsive timeline (#54)
    * chore: added npmrc configs
    
    * chore: deploy issues resolved
    
    * fix: sponsors overlap
    
    * animated background
    
    * updated background for sm
    
    * fix: responsiveness of timeline
    
    ---------
    
    Co-authored-by: Kevin Paul Jacob <kpjtofficial@gmail.com>
    Co-authored-by: Kunal Keshan <kunalkeshan12@gmail.com>
    Co-authored-by: VivekSGopalakrishnan <gv8464@srmist.edu.in>

--- components/landing/Sponsors.tsx
+++ components/landing/Sponsors.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 function Sponsors() {
   const sponsors = [
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-    { url: '/assets/layouts/logo.svg', name: "IEEE Logo" },
-  ]
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+    { url: "/assets/layouts/logo.svg", name: "IEEE Logo" },
+  ];
 
   return (
     <div className="w-full flex flex-col gap-6 items-center mt-20 overflow-hidden">
@@ -21,17 +21,31 @@ function Sponsors() {
       </div>
       <section className="w-full">
         <div className="relative flex overflow-x-hidden mt-6 bg-white/20">
-              <div className="flex py-5 animate-marquee whitespace-nowrap">
-                  {sponsors.map((sponsor, index) => (
-                      <Image key={index} className="mx-4" src={sponsor.url} alt={sponsor.name} width={200} height={200}/>
-                  ))}
-              </div>
-              <div className="flex absolute top-0 py-5 animate-marquee2 whitespace-nowrap">
-                  {sponsors.map((sponsor, index) => (
-                      <Image key={index} className="mx-4" src={sponsor.url} alt={sponsor.name} width={200} height={200}/>
-                  ))}
-              </div>
+          <div className="flex py-5 animate-marquee whitespace-nowrap">
+            {sponsors.map((sponsor, index) => (
+              <Image
+                key={index}
+                className="mx-4"
+                src={sponsor.url}
+                alt={sponsor.name}
+                width={200}
+                height={200}
+              />
+            ))}
           </div>
+          <div className="lg:flex absolute top-0 py-5 animate-marquee2 whitespace-nowrap ml-28 hidden">
+            {sponsors.map((sponsor, index) => (
+              <Image
+                key={index}
+                className="mx-4"
+                src={sponsor.url}
+                alt={sponsor.name}
+                width={200}
+                height={200}
+              />
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   );
