1502622c52363443468f2f3d41b78f4776607580
Kunal Keshan <kunalkeshan12@gmail.com>
2023-07-15 18:41:02 +0530

    chore: testing with hackathon button

--- components/hackathon/Hero.tsx
+++ components/hackathon/Hero.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 const HackathonHero = () => {
-	return <div>HackathonHero</div>;
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src = 'https://apply.devfolio.co/v2/sdk.js';
+		script.async = true;
+		script.defer = true;
+		document.body.appendChild(script);
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
+
+	return (
+		<section className='w-full min-h-screen'>
+			<div
+				className='apply-button w-[312px] h-[44px]'
+				data-hackathon-slug='technozest'
+				data-button-theme='light'
+			></div>
+		</section>
+	);
 };
 
 export default HackathonHero;
--- pages/hackathon.tsx
+++ pages/hackathon.tsx
@@ -1,10 +1,20 @@
-import Prizes from "@/components/hackathon/Prizes";
+import HackathonHero from '@/components/hackathon/Hero';
 
-function hackathon() {
+function HackathonPage() {
 	return (
-		<Prizes/>
+		<main className='relative flex flex-col justify-start items-center w-full min-h-screen overflow-hidden'>
+			<video
+				id='videoBg'
+				className='-z-10 absolute top-0'
+				autoPlay
+				loop
+				muted
+			>
+				<source src='/assets/landing/bgVideo.mp4' />
+			</video>
+			<HackathonHero />
+		</main>
 	);
 }
 
-export default hackathon;
-
+export default HackathonPage;
