0695b1f8d34b4f681036f32bf5cf364a5bed5860
kunalkeshan <kunalkeshan12@gmail.com>
2023-07-16 15:43:12 +0530

    chore: hackathon page hero section

--- components/hackathon/Hero.tsx
+++ components/hackathon/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useEffect } from 'react';
 
 const HackathonHero = () => {
@@ -13,12 +14,26 @@ const HackathonHero = () => {
 	}, []);
 
 	return (
-		<section className='w-full min-h-screen'>
+		<section className='relative flex flex-col md:justify-center items-center w-full min-h-screen gap-8'>
+			<div className='w-full md:w-1/2 mt-40 md:mt-0'>
+				<Image
+					src={'/assets/technozest.png'}
+					alt='Technozest'
+					width={100}
+					height={100}
+					className='w-full h-auto object-contain'
+					priority
+					unoptimized
+				/>
+			</div>
 			<div
 				className='apply-button w-[312px] h-[44px]'
 				data-hackathon-slug='technozest'
 				data-button-theme='light'
 			></div>
+			<h3 className='md:text-2xl sm:text-xl text-lg text-center px-10'>
+				Encounter your canon event across the TechnoVerse
+			</h3>
 		</section>
 	);
 };
